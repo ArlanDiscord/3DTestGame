e690b6ed1cbd8a6a11c68d5f825aebf0313d8865
ArlanDiscord <90557945+ArlanDiscord@users.noreply.github.com>
2024-02-11 14:00:12 +0600

    Update
    - New character
    - With Animation

---
+++ Assets/AnimController.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    Animator anim => GameObject.Find("Character").GetComponent<Animator>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", true);
+            }
+            anim.SetBool("isCrouching", false);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", true);
+            }
+            anim.SetBool("isCrouching", false);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", true);
+            }
+            anim.SetBool("isCrouching", false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", true);
+            }
+            anim.SetBool("isCrouching", false);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isCrouching", false);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isCrouching", true);
+        }
+        if (!Input.GetKey(KeyCode.W)&&
+            !Input.GetKey(KeyCode.S)&&
+            !Input.GetKey(KeyCode.A)&&
+            !Input.GetKey(KeyCode.D)&&
+            !Input.GetKey(KeyCode.LeftControl)&&
+            !Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isCrouching", false);
+        }
+    }
+}
